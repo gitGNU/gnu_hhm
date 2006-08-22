64e6ceac94b51e42ef9986744eb9f150322f6f93
Paul Wise <pabs3@bonedaddy.net>
2006-08-22 06:04:16 +0000

    Update copyright/licencing information

--- hhm.c
+++ hhm.c
@@ -1,10 +1,11 @@
 /*
 hhm -- make an ITS file and in the future a CHM file
-Copyright (C) 2002 Pabs
+Copyright (C) 2002, 2005, 2006 Paul Wise
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation; version 2 only
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
